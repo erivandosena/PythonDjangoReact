ec32598ad45f3b205842dfac132ff8aa904e81fb
Erivando Sena <erivandosena@gmail.com>
2022-03-28 10:48:06 -0300

    Criando a API

--- backend/backend_api/admin.py
+++ backend/backend_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list = ('title', 'author', 'isbn')
+
+    admin.site.register(Book)
